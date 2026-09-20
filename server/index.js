0c408d430251d25e6f1f08a031ffd4a556827c1e
ammaarun <ammaarunkumar2003@gmail.com>
2026-09-20 21:55:52 +0530

    fix: update Express v5 SPA catch-all route syntax to app.get('(.*)')

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ const distPath = path.join(__dirname, '../dist');
 app.use(express.static(distPath));
 
 // SPA catch-all route for client-side routing
-app.get('*', (req, res, next) => {
+app.get('(.*)', (req, res, next) => {
   if (req.path.startsWith('/api')) return next();
   res.sendFile(path.join(distPath, 'index.html'), (err) => {
     if (err) next();
